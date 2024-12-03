9bc696bdb434f20b9d78ce5b31a447414a557f70
mark-gibson-44 <mjgibson44@outlook.com>
2024-12-03 21:33:52 +0000

    Created template for day4

---
+++ day4/day.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+
+void parseInput()
+{
+    std::ifstream file("day4Input.txt");
+    std::string str; 
+    int res = 0;
+    bool doMul = true;
+    while (std::getline(file, str))
+    {
+
+    }
+
+}
+
+int main()
+{
+    parseInput();
+}
